a8fb07db43bc1d8aba3e8ac330a8e7d56d5947e5
Keval <keval9464@gmail.com>
2023-07-21 15:00:25 +0530

    2nd day commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import "./home.css"
+
+const Home = () => {
+    return (
+        <>
+            <div className="firstDiv">
+                <input type="text" className="inputTag"  />
+                <input type="button" value="Submit" className="subButton" />
+            </div>
+        </>
+    )
+}
+
+export default Home;
