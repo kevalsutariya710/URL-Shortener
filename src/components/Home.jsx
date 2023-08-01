2d648eae6498070ef8d852abd06736e5502eddde
Keval <keval9464@gmail.com>
2023-08-01 16:58:25 +0530

    5th commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,20 +12,20 @@ const Home = ({ setInputValue }) => {
 
     return (
         <>
-            <div className="content-center">
+            <h1>URL Shortener</h1>
 
-                <h1 className="flex justify-center items-center">URL Shortener</h1>
-                <input
-                    type="text"
-                    className="inputTag"
-                    placeholder="Past The Link"
-                    value={value}
-                    onChange={e => setValue(e.target.value)}
-                />
-                <input type="button" value="Submit" className="subButton"
-                    onClick={handleClick}
-                />
-            </div>
+
+
+            <input
+                type="search"
+                id="default-search"
+                placeholder="Enter or Pest URL...."
+                value={value}
+                onChange={e => setValue(e.target.value)}
+                required />
+
+            <button type="submit" onClick={handleClick}> Sort </button>
+
         </>
     )
 }
--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -34,7 +34,6 @@ const Result = ({ inputValue }) => {
             </CopyToClipboard>
         </>
 
-
     )
 }
 
